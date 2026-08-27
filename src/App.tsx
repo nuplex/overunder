71d4c0bd4491d46aa0a4a16a54920e5fe2320ab2
Chijioke Okonkwo <chokon@umd.edu>
2026-08-27 01:00:58 -0400

    fix toggle positions

--- src/App.tsx
+++ src/App.tsx
@@ -594,7 +594,7 @@ function Toggle({
   isActive: boolean
 }){
   let fallback = '-';
-  const fromBottom = index == 1 ? 24 : (24 + 40) * index;
+  const fromBottom = index == 1 ? 24 : (24 * index) + 40;
   const background = isActive ? "rgba(255,215,0,0.95)" : "rgba(230,230,230,0.95)";
   const textColor = isActive ? "#333" : undefined;
 
