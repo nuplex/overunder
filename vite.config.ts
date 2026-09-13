121810f10a0a5de8a7eb65e851cd34f54058256e
Chijioke Okonkwo <chokon@umd.edu>
2026-09-13 12:37:39 -0400

    test vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   build: {
     emptyOutDir: true,
   },
